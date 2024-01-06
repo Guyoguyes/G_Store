9fc8f8009bc94d6a1c965b6929aca75115838435
Guyoguyes <g.abduba43@gmail.com>
2024-01-06 15:50:29 +0300

    colors added

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  CustomColors._();
+
+  // App Basic colors
+  static const Color primary = Color(0xFF4b68ff);
+  static const Color secondary = Color(0xFFFFE24B);
+  static const Color accent = Color(0xFFb0c7ff);
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF6c757d);
+  static const Color textWhite = Colors.white;
+
+  // Background Colors
+  static const Color light = Color(0xFFf6f6f6);
+  static const Color dark = Color(0xFF272727);
+  static const Color primaryBackground = Color(0xFFf3f5ff);
+
+  // Background Container Colors
+  static const Color lightContainer = Color(0xFFf6f6f6);
+  static  Color darkContainer = Colors.white.withOpacity(0.1);
+}
